799222f7a3400080aa6e2ff7446cc5665d2c6306
Bismarck <brunowasborn2rock@gmail.com>
2023-11-19 15:38:14 -0300

    back 2 the usual

---
+++ Aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel =  parseInt(Math.random() * (120 - 1) + 1)
+console.log(`A velocidade do seu carro é ${vel} km/h!`)
+if(vel > 60){
+    console.log(`Você ultrapassou a velocidade permitida! Multado!`)
+}
+console.log(`Diriga sempre usando cinto de segurança!`)
---
+++ Aula11/ex009.js
@@ -0,0 +1,8 @@
+var país = 'Brasil'
+console.log(`Vivendo no(s) ${país}!`)
+if(país == 'Brasil'){
+    console.log('Voce é brasileiro!')
+} 
+else{
+    console.log('Você é estrangeiro!')
+}
